026fdb66b907fb040dd8aa765d88fbd32ec6f33f
MyBrianHurt5 <157983148+MyBrianHurt5@users.noreply.github.com>
2024-03-09 11:42:49 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,34 @@
+#Welcome to the official Chaos Facility Simulator
+#Build your Faclity, manage your Reactor and purchase powerful upgrades!
+
+print ("Edition\nALPHA-0-0-1")
+
+#code begins
+#variable setup
+
+ENERGY=0 #needed to profit and run machines
+PPS=0 #profit per second
+MONEY=0 #needed to purchase upgrades
+BATTERY=0 #backup power to restart reactor
+LEVEL=0 #unlocks new upgrades
+SPECIALS=0 #needed to build powerful upgrades
+SPECIALS_PRICE=0 #decreases with researcher level
+
+Reactor_level=0 #increases energy produced
+Reactor_stability=0 #needed to prevent meltdown
+
+Grid_level=0 #increases profit
+Grid_power=0 #power diverted to energy grid
+Factory_level=0 #increases resource production 
+Factory_power=0 #power diverted to resource factories
+Research_level=0 #increases level
+Research_power=0 #power diverted to research labs
+Engineer_level=0 #increases reactor stability
+Engineer_power=0 #power diverted to engineers
+Security_level=0 #increases protection from disasters
+Security_power=0 #power diverted to security stations
+
+Steel=0 #used for general construction
+Concrete=0 #used in large amounts for construction
+Electronics=0 #used in complex machine construction
 
